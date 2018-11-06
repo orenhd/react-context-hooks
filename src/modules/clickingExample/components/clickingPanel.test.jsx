adfd73956f7649bb87b4cd66f5ad619cbfd9e8d0
Oren Hadar <orenhd123@gmail.com>
2018-11-06 15:57:17 +0200

    add enzyme

--- src/modules/clickingExample/components/clickingPanel.test.jsx
+++ src/modules/clickingExample/components/clickingPanel.test.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+
+import ClickingPanel from './clickingPanel';
 
 describe('clickingPanel', () => {
   it('test', () => {
     expect(true).toBeTruthy();
   })
+
+  it('should render correctly', () => {
+    const component = shallow(<ClickingPanel
+      clickingData={{}}
+      homeButtonClickedHandler={jest.fn()}
+      homeButtonClickedOutsideHandler={jest.fn()}
+    />);
+
+    expect(component).toMatchSnapshot();
+  });
 });
